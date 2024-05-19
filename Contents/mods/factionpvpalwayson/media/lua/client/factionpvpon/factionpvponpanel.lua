cfca1d8a41c751b15ee681419361fd0e31c08ee6
Vorshim92 <vorshim92@gmail.com>
2024-05-19 02:19:07 +0200

    fix and refactor

--- Contents/mods/factionpvpalwayson/media/lua/client/factionpvpon/factionpvponpanel.lua
+++ Contents/mods/factionpvpalwayson/media/lua/client/factionpvpon/factionpvponpanel.lua
@@ -13,21 +13,26 @@
 local ISFactionUIext_initialise = ISFactionUI.initialise;
 
 function ISFactionUI:initialise(...)
-           -- Chiama la funzione originale
-
+    -- Chiama la funzione originale
     ISFactionUIext_initialise(self, ...);
 
-
+    -- Controlla il valore di "FactionPvP.Alwayson" nelle opzioni del sandbox
     if getSandboxOptions():getOptionByName("FactionPvP.Alwayson"):getValue() then
-        self.player:setFactionPvp(true);
-        sendPlayerStatsChange(self.player);
-    else 
-        self.player:setFactionPvp(false);
-        sendPlayerStatsChange(self.player);
+        -- Se il PvP di fazione non è già attivo, attivalo
+        if not self.player:isFactionPvp() then
+            self.player:setFactionPvp(true);
+            sendPlayerStatsChange(self.player);
+        end
+    else
+        -- Se il PvP di fazione è attivo, disattivalo
+        if self.player:isFactionPvp() then
+            self.player:setFactionPvp(false);
+            sendPlayerStatsChange(self.player);
+        end
     end
+
+    -- Disabilita l'opzione PvP di fazione nell'interfaccia
     self.factionPvp.enable = false;
+    -- Aggiorna lo stato selezionato del PvP di fazione
     self.factionPvp.selected[1] = self.player:isFactionPvp();
-
 end
-
-
